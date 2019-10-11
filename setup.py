8c947bf35e6777c131930523f8ade6e52ad064b2
mattian7741 <github@mattian.com>
2019-10-11 13:34:52 -0400

    had to change from smart to active

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-  name = 'smart', # How you named your package folder (MyLib)
-  packages = ['smart'], # Chose the same as "name"
+  name = 'active', # How you named your package folder (MyLib)
+  packages = ['active'], # Chose the same as "name"
   version = '0.0.4-alpha', # Start with a small number and increase it with every change you make
   license='MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple Microservice Application Runtime Toolkit', # Give a short description about your library
   author = 'Matthew Hansen', # Type in your name
-  author_email = 'smart@mattian.com', # Type in your E-Mail
+  author_email = 'active@mattian.com', # Type in your E-Mail
   url = 'https://github.com/mattian7741/zulu', # Provide either the link to your github or to your website
   download_url = 'https://github.com/mattian7741/zulu/archive/v0.0.4-alpha.tar.gz', # github release url
   keywords = ['EXECUTE', 'MICROSERVICE', 'LAMBDA'], # Keywords that define your package best
@@ -24,7 +24,7 @@ setup(
   ],
   entry_points={
     'console_scripts': [
-      'smart=smart.__main__:run'
+      'active=active.__main__:run'
     ]
   }
 
