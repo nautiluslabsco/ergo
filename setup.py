2444e61b8743157317d1fa1fafce7d2437b999c2
mattian7741 <github@mattian.com>
2019-10-11 00:39:22 -0400

    updated download-url in setup.py to reflect actual release url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Matthew Hansen', # Type in your name
   author_email = 'zulu@mattian.com', # Type in your E-Mail
   url = 'https://github.com/mattian7741/zulu', # Provide either the link to your github or to your website
-  download_url = 'https://github.com/mattian7741/zulu/archive/v_01.tar.gz', # I explain this later on
+  download_url = 'https://github.com/mattian7741/zulu/archive/v0.0.1-alpha.tar.gz', # I explain this later on
   keywords = ['EXECUTE', 'MICROSERVICE', 'LAMBDA'], # Keywords that define your package best
   install_requires=[ # I get to this in a second
           'validators',
