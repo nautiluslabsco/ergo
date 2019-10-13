636adfd40fc085d106c4bbcd57ae6e13f38c9e21
mattian7741 <github@mattian.com>
2019-10-12 23:11:34 -0400

    tried removing ergo as a debugging step

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
   description = 'Execution Runtime Graph Orchestration', # Give a short description about your library
   author = 'Matthew Hansen', # Type in your name
   author_email = 'ergo@mattian.com', # Type in your E-Mail
-  url = 'https://github.com/mattian7741/zulu', # Provide either the link to your github or to your website
+  url = 'https://github.com/mattian7741/ergologic', # Provide either the link to your github or to your website
   download_url = f'https://github.com/mattian7741/ergologic/archive/v{VERSION}.tar.gz', # github release url
-  keywords = ['EXECUTE', 'MICROSERVICE', 'LAMBDA'], # Keywords that define your package best
+  keywords = ['execute', 'microservice', 'lambda'], # Keywords that define your package best
   install_requires=[ # dependencies
     'Click',
   ],
