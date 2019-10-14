8eea6bac8fd93bfc8034934e522f7603995087af
mattian7741 <github@mattian.com>
2019-10-13 20:54:37 -0400

    changed tag vs VERSION test back to == in setup.py to troubleshoot circleci integration issue

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class VerifyVersionCommand(install):
     def run(self):
         tag = os.getenv('CIRCLE_TAG')
 
-        if tag == VERSION:
+        if tag != VERSION:
             info = "Git tag: {0} does not match the version of this app: {1}".format(
                 tag, VERSION
             )
