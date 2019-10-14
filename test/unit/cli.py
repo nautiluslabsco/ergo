cd7f804d6fede7a416c25e06a02188be97c5589e
mattian7741 <github@mattian.com>
2019-10-13 22:23:03 -0400

    barebones test

--- test/unit/cli.py
+++ test/unit/cli.py
@@ -1,28 +1,8 @@
 import os
-import pprint
 import unittest
 from unittest.mock import MagicMock
 
-from circleci.api import Api
-from circleci.error import BadKeyError, BadVerbError, InvalidFilterError
 
-
-class TestCircleCIApi(unittest.TestCase):
-
-    def setUp(self):
-        self.c = Api(os.getenv('CIRCLE_TOKEN'))
-
-    def loadMock(self, filename):
-        """helper function to open mock responses"""
-        filename = 'tests/mocks/{0}'.format(filename)
-
-        with open(filename, 'r') as f:
-            self.c._request = MagicMock(return_value=f.read())
-
+class TestCli(unittest.TestCase):
     def test_bad_verb(self):
-
-        with self.assertRaises(BadVerbError) as e:
-            self.c._request('BAD', 'dummy')
-
-        self.assertEqual('BAD', e.exception.argument)
-        self.assertIn('DELETE', e.exception.message)
+        assert(True)
