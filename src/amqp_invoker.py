12f3c59460683ca84477d0feb22a39f6bffccc0e
mattian7741 <github@mattian.com>
2020-11-21 20:17:35 -0500

    POC amqp executor

--- src/amqp_invoker.py
+++ src/amqp_invoker.py
@@ -8,8 +8,6 @@ from src.payload import Payload
 
 # content_type: application/json
 # {"x":5,"y":7}
-
-
 class AmqpInvoker(Invoker):
     """Summary."""
 
@@ -20,14 +18,13 @@ class AmqpInvoker(Invoker):
         queue_name = self._invocable.reference
         channelx.queue_declare(queue=queue_name)
 
-        def handler(channel, method, properties, body) -> None:   # type: ignore
+        def handler(channel, method, properties, body) -> None: # type: ignore
             data_out: Payload = Payload()
             data_in: Payload = Payload(dict(json.loads(body.decode('utf-8'))))
 
             self._invocable.invoke(data_out, data_in)
 
             channelx.basic_publish(exchange='primary', routing_key='a.b.c', body=str(data_out))
-
         channelx.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=handler)
 
         channelx.start_consuming()
