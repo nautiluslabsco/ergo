71fec925006ef5e7154e991b06bd9d0825e0c043
mattian7741 <github@mattian.com>
2021-02-21 00:40:58 -0500

    fixing versions

--- src/version.py
+++ src/version.py
@@ -24,40 +24,40 @@ def get_version() -> str:
 # wtc && incr => not ok
 # changes && match => not ok
 # changes && incr => ok
-# def main_x():
-#     """Summary."""
-#     ver: str = get_version()
-#     tag: str = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8')
-#     status: str = subprocess.check_output(['git', 'status']).decode('utf-8')
-#     try:
-#         if tag.index(ver) == 0 and 'working tree clean' not in status:  # if version hasn't changed
-#             print('Version must be incremented with changes to codebase')
-#             sys.exit(1)
-#         else:
-#             # print('not status')
-#             # sys.exit(0)
-#             print(get_version())
-#     except ValueError:
-#         print(get_version())
-
-
 def main() -> None:
     """Summary."""
     ver: str = get_version()
     tag: str = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8')
     status: str = subprocess.check_output(['git', 'status']).decode('utf-8')
-    clean = 'working tree clean' in status
-    match = False
     try:
-        match = tag.index(ver) == 0
+        if tag.index(ver) == 0 and 'working tree clean' not in status:  # if version hasn't changed
+            print('Version must be incremented with changes to codebase')
+            sys.exit(1)
+        else:
+            # print('not status')
+            # sys.exit(0)
+            print(get_version())
     except ValueError:
-        pass
-
-    if clean and not match or match and not clean:
-        print('Version must be incremented with changes to codebase')
-        sys.exit(1)
-    else:
         print(get_version())
+
+
+# def main() -> None:
+#     """Summary."""
+#     ver: str = get_version()
+#     tag: str = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8')
+#     status: str = subprocess.check_output(['git', 'status']).decode('utf-8')
+#     clean = 'working tree clean' in status
+#     match = False
+#     try:
+#         match = tag.index(ver) == 0
+#     except ValueError:
+#         pass
+
+#     if clean and not match or match and not clean:
+#         print('Version must be incremented with changes to codebase')
+#         sys.exit(1)
+#     else:
+#         print(get_version())
 
 
 if __name__ == '__main__':
