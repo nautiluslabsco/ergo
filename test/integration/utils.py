4c3bdffec17bd5faf4fe558c1b373fb0ee441c65
Zach Schubert <zschubert@nautiluslabs.co>
2021-09-24 09:38:47 -0400

    with_ergo docstring

--- test/integration/utils.py
+++ test/integration/utils.py
@@ -1,16 +1,25 @@
 import multiprocessing
 import functools
-from typing import Callable, Optional
+from typing import Callable
 from src.ergo_cli import ErgoCli
 
 
-def with_ergo(command: str, target: str, namespace: Optional[str] = None):
+def with_ergo(command: str, *ergo_args: str):
+    """
+    This decorator adds setup code to the wrapped function that starts a temporary ergo worker in a child process.
+    The worker is terminated prior to the wrapped function returning.
+
+    Args:
+        command (str): An ergo command, e.g. start, amqp or http.
+        *ergo_args (str): Positional arguments to pass to ergo.
+    :return:
+    """
     def decorator(fn: Callable):
         @functools.wraps(fn)
         def wrapped(*args, **kwargs):
             ergo_process = multiprocessing.Process(
                 target=getattr(ErgoCli(), command),
-                args=(target, namespace,),
+                args=ergo_args,
             )
             ergo_process.start()
             try:
