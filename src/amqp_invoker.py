a1803c6afde525ed868ece11785cf892c590fb46
ahuman-bean <andrew@nautiluslabs.co>
2021-10-08 13:56:22 -0400

    feat(amqp_invoker): Basic threaded consumer for long-running work

--- src/amqp_invoker.py
+++ src/amqp_invoker.py
@@ -1,8 +1,11 @@
 """Summary."""
 import json
-from typing import Tuple
+import threading
+from typing import Any, List, Tuple
 from urllib.parse import urlparse
+from markupsafe import functools
 import pika
+from src.function_invocable import FunctionInvocable
 from src.invoker import Invoker
 from src.types import TYPE_PAYLOAD
 
@@ -20,7 +23,19 @@ def set_param(host: str, param_key: str, param_val: str) -> str:
 class AmqpInvoker(Invoker):
     """Summary."""
 
-    def connect(self) -> Tuple[pika.adapters.blocking_connection.BlockingChannel, str, str]:
+    def __init__(self, invocable: FunctionInvocable) -> None:
+        super().__init__(invocable)
+        self.threads: List[threading.Thread] = []
+
+    def start(self) -> int:
+        """Summary."""
+        connection, channel, queue_name, queue_name_error = self.connect()
+        wrapper = functools.partial(self.on_message, args=(connection, self.threads, queue_name_error))
+        channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=wrapper)
+        channel.start_consuming()
+        return 0
+
+    def connect(self) -> Tuple[pika.BlockingConnection, pika.adapters.blocking_connection.BlockingChannel, str, str]:
         """Connect to a rabbit broker."""
         heartbeat = self._invocable.config.heartbeat
         host = set_param(self._invocable.config.host, 'heartbeat', str(heartbeat)) if heartbeat else self._invocable.config.host
@@ -34,37 +49,39 @@ class AmqpInvoker(Invoker):
         channel.queue_declare(queue=queue_name_error)
         channel.exchange_declare(exchange_name, exchange_type='topic', passive=False, durable=True, auto_delete=False, internal=False, arguments=None)
         channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=str(self._invocable.config.subtopic))
-        return channel, queue_name, queue_name_error
+        return connection, channel, queue_name, queue_name_error
 
-    def start(self) -> int:
-        """Summary."""
-        channel, queue_name, queue_name_error = self.connect()
+    def on_message(self, channel, method, properties, body, args) -> None:  # type: ignore
+        connection, threads, routing_key = args
+        t = threading.Thread(target=self.do_work, args=(connection, channel, body, method.delivery_tag, routing_key))
+        t.start()
+        threads.append(t)
 
-        def handler(channel, method, properties, body) -> None:  # type: ignore
-            """Summary.
+    def do_work(self, connection, channel, body, delivery_tag, routing_key) -> None:  # type: ignore
+        # thread_id = threading.get_ident()
+        # TODO: can be used for tracing
 
-            Args:
-                channel (TYPE): Description
-                method (TYPE): Description
-                properties (TYPE): Description
-                body (TYPE): Description
-            """
-            data_in: TYPE_PAYLOAD = dict(json.loads(body.decode('utf-8')))
-            try:
-                for data_out in self._invocable.invoke(data_in["data"]):
-                    payload = {
-                        "data": data_out,
-                        "key": str(self._invocable.config.pubtopic),
-                        "log": data_in.get("log", []),
-                    }
-                    channel.basic_publish(exchange=self._invocable.config.exchange,
-                                          routing_key=str(self._invocable.config.pubtopic), body=json.dumps(payload))
-            except Exception as err:  # pylint: disable=broad-except
-                data_in['error'] = str(err)
-                channel.basic_publish(exchange='', routing_key=queue_name_error, body=json.dumps(data_in))
+        # Perform actual work
+        data_in: TYPE_PAYLOAD = dict(json.loads(body.decode('utf-8')))
+        try:
+            for data_out in self._invocable.invoke(data_in["data"]):
+                payload = {
+                    "data": data_out,
+                    "key": str(self._invocable.config.pubtopic),
+                    "log": data_in.get("log", []),
+                }
+                channel.basic_publish(exchange=self._invocable.config.exchange,
+                                      routing_key=str(self._invocable.config.pubtopic), body=json.dumps(payload))
+        except Exception as err:  # pylint: disable=broad-except
+            data_in['error'] = str(err)
+            channel.basic_publish(exchange='', routing_key=routing_key, body=json.dumps(data_in))
 
-        channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=handler)
+        callback = functools.partial(self.ack, channel, delivery_tag)
+        connection.add_callback_threadsafe(callback)
 
-        channel.start_consuming()
-
-        return 0
+    def ack(self, channel, delivery_tag) -> None:  # type: ignore
+        if channel.is_open:
+            channel.basic_ack(delivery_tag)
+        else:
+            # TODO: can log or raise exception
+            pass
