1a800f102eb58a5d1577b4043b1516f2aa6a0b92
ahuman-bean <andrew@nautiluslabs.co>
2021-11-12 09:16:06 -0500

    fix(amqp): Ack message before processing to satisfy broker consumer timeout

--- src/amqp_invoker.py
+++ src/amqp_invoker.py
@@ -127,8 +127,13 @@ class AmqpInvoker(Invoker):
             await queue_error.bind(exchange=exchange, routing_key=f'{self.queue_name}_error')
 
             async for message in queue:
-                async with message.process():
-                    yield dict(json.loads(message.body.decode('utf-8')))
+                data_in: TYPE_PAYLOAD = dict(json.loads(message.body.decode('utf-8')))
+                # Subsequent processing work could still fail, but message delivery is considered to be successful since the `message` was not structurally malformed
+                # Therefore, acknowledgement is sent upon successful receipt and deserialization of `message`
+                # This is performed prior to processing due to the `consumer_timeout` (default 30 minutes) on the broker side
+                # See: https://www.rabbitmq.com/consumers.html#acknowledgement-timeout
+                message.ack()
+                yield data_in
 
     async def publish(self, channel_pool: aio_pika.pool.Pool[aio_pika.RobustChannel], message: aio_pika.Message, routing_key: str) -> None:
         """
--- src/types.py
+++ src/types.py
@@ -4,7 +4,7 @@ Attributes:
     TYPE_RETURN (TYPE): Description
 
 """
-from typing import Any
+from typing import Any, Dict
 
 TYPE_RETURN = Any
-TYPE_PAYLOAD = Any
+TYPE_PAYLOAD = Dict[str, Any]
