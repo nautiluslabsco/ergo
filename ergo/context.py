8db15e41be75eb67ad40a070aa42cc7d4a614bf1
Zach Schubert <zschubert@nautiluslabs.co>
2022-01-24 16:42:58 -0500

    documentation

--- ergo/context.py
+++ ergo/context.py
@@ -16,12 +16,38 @@ class Context:
         self._results_stream = results_stream
 
     def request(self, topic: str, **kwargs):
+        """
+        Publish a message with the given topic and data. Receiving components may call `Context.respond` to
+        reply directly to the sending instance.
+
+        >>> import random
+        >>> x = random.random()
+        >>> def requester(context: Context, r=None):
+        ...     global x
+        ...     if r:
+        ...         assert r == x
+        ...     else:
+        ...         context.request('responder', r=x)
+        ...
+        >>> def responder(context: Context, r):
+        ...     context.respond(r=r)
+        ...
+
+        """
         with self._transaction():
             self._stack.set_reply_to(instance_id())
             self._results_stream.send(Payload(data=kwargs or None, key=f"{topic}.request", stack=self._stack))
 
     def respond(self, data=None, **kwargs):
+        """
+        See docstring for Context.request.
+
+        """
         if self._stack.get_reply_to() == instance_id():
+            # We must be inside a recursive request stack, and the current transaction is associated with a request call
+            # that we made in a prior invocation. If its parent is null, then we've reached the 'base case' and should
+            # terminate recursion by publishing to our current pubtopic. Otherwise, the parent transaction is associated
+            # with the request that we're responding to now.
             self._close_transaction()
         if self._stack:
             key = self._stack.get_reply_to()
