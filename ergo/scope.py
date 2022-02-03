afdb6c0f750e812549eaa94a126fdca3cf9e4f94
Zach Schubert <zschubert@nautiluslabs.co>
2022-02-03 15:49:55 -0500

    add methods for getting and setting arbitrary scope data

--- ergo/scope.py
+++ ergo/scope.py
@@ -9,4 +9,11 @@ from ergo.util import uniqueid
 @dataclass
 class Scope:
     id: str = field(default_factory=uniqueid)
+    data: dict = field(default_factory=dict)
     parent: Optional[Scope] = None
+
+    def store(self, key: str, value):
+        self.data[key] = value
+
+    def retrieve(self, key):
+        return self.data.get(key)
