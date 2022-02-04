db308e07f84e0de020aee96bb157bf962e7440f2
Zach Schubert <zschubert@nautiluslabs.co>
2022-02-04 11:49:13 -0500

    data -> metadata

--- ergo/scope.py
+++ ergo/scope.py
@@ -9,11 +9,11 @@ from ergo.util import uniqueid
 @dataclass
 class Scope:
     id: str = field(default_factory=uniqueid)
-    data: dict = field(default_factory=dict)
+    metadata: dict = field(default_factory=dict)
     parent: Optional[Scope] = None
 
     @property
     def subscribers(self) -> List:
-        if "subscribers" not in self.data:
-            self.data["subscribers"] = []
-        return self.data["subscribers"]
+        if "subscribers" not in self.metadata:
+            self.metadata["subscribers"] = []
+        return self.metadata["subscribers"]
