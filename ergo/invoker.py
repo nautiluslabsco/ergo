79fb96b20e438b9ed1326faeb6bf7c0d8072a73f
Zach Schubert <zschubert@nautiluslabs.co>
2022-02-07 13:52:51 -0500

    add Invoker.defer_termination

--- ergo/invoker.py
+++ ergo/invoker.py
@@ -1,5 +1,7 @@
 """Summary."""
+import signal
 from abc import ABC, abstractmethod
+from contextlib import contextmanager
 from typing import Generator
 
 from ergo.function_invocable import FunctionInvocable
@@ -18,6 +20,8 @@ class Invoker(ABC):
         """
         super().__init__()
         self._invocable = invocable
+        self._signum = None
+        self._terminating = False
 
     @abstractmethod
     def start(self) -> int:
@@ -31,3 +35,20 @@ class Invoker(ABC):
 
     def invoke_handler(self, message_in: Message) -> Generator[Message, None, None]:
         yield from self._invocable.invoke(message_in)
+
+    @contextmanager
+    def defer_termination(self):
+        """
+        Use this context manager to temporarily postpone SIGTERM handling.
+        """
+        prev_handler = signal.signal(signal.SIGTERM, self._sigterm_handler)
+        try:
+            yield
+        finally:
+            signal.signal(signal.SIGTERM, prev_handler)
+            if self._signum:
+                signal.raise_signal(signal.SIGTERM)
+
+    def _sigterm_handler(self, signum, _):
+        self._signum = signum
+        self._terminating = True
