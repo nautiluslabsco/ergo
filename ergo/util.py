d6c42980d7f264174a15652909f7798b18db6f93
zachschubert <88679481+zachschubert@users.noreply.github.com>
2022-02-15 13:03:38 -0500

    Schubert/reorganize amqp invoker pd 612 (#60)
    * reorganize AmqpInvoker
    
    * acquire fresh channel at publish-time
    
    * black
    
    * Revert "acquire fresh channel at publish-time"
    
    This reverts commit ba3fa344ef0c918d21f43fd32d893fbe5aa7393d.
    
    * stash
    
    * deleteme: test_ping
    
    * Revert "deleteme: test_ping"
    
    This reverts commit 0c7cb6c325a23fd853b4c505f64fa912a0a0d056.
    
    * add Invoker.defer_termination
    
    * move amqp_message.ack() into defer_termination block
    
    * fix defer_termination
    
    * lint
    
    * revert to acking late
    
    * MAX_THREADS -> 4

--- ergo/util.py
+++ ergo/util.py
@@ -1,6 +1,8 @@
 """Convenience Funcs for handling errors, logging, and monitoring."""
 import re
+import signal
 import sys
+import threading
 import time
 import traceback
 from types import FrameType, TracebackType
@@ -126,3 +128,28 @@ def extract_from_stack(exc: BaseException) -> Tuple[Optional[str], Optional[str]
         if len(matches) == 3:  # for mypy
             return matches[0], matches[1], matches[2]
     return None, None, None
+
+
+_shutdown = threading.Event()
+_termination_pending = threading.Event()
+
+
+class defer_termination:
+    """
+    Use this context manager to temporarily postpone shutdown via SIGTERM.
+    """
+
+    def __enter__(self):
+        if _termination_pending.is_set():
+            _shutdown.wait()
+        self._signum = None
+        signal.signal(signal.SIGTERM, self._sigterm_handler)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        signal.signal(signal.SIGTERM, 0)
+        if self._signum:
+            signal.raise_signal(self._signum)
+
+    def _sigterm_handler(self, signum, _):
+        _termination_pending.set()
+        self._signum = signum
