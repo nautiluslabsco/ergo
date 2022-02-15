8b20ccb96ef3cf99b7df7d5398da09d7ce529f02
jaechoe <jae@nautiluslabs.co>
2022-02-15 13:24:35 -0500

    autopep8

--- ergo/scope.py
+++ ergo/scope.py
@@ -9,7 +9,7 @@ from ergo.util import uniqueid
 @dataclass
 class Scope:
     id: str = field(default_factory=uniqueid)
+    data: dict = field(default_factory=dict)
     reply_to: Optional[str] = None
     parent: Optional[Scope] = None
-    data: dict = field(default_factory=dict)
 
