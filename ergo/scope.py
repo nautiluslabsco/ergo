06d07802ea44eb1369cf3995134d362fdd6566d9
Jae <52420646+jaechoe@users.noreply.github.com>
2022-02-16 11:13:18 -0500

    [PD-614] feat(ergo): in message scope (#63)
    * [PD-614] feat(ergo): in message scope
    
    Expose methods via Context to be able to set/get scope.data.
    
    * autopep8
    
    * autopep8 forreal
    
    * Update ergo/context.py
    
    Co-authored-by: zachschubert <88679481+zachschubert@users.noreply.github.com>
    
    Co-authored-by: zachschubert <88679481+zachschubert@users.noreply.github.com>

--- ergo/scope.py
+++ ergo/scope.py
@@ -9,5 +9,6 @@ from ergo.util import uniqueid
 @dataclass
 class Scope:
     id: str = field(default_factory=uniqueid)
+    data: dict = field(default_factory=dict)
     reply_to: Optional[str] = None
     parent: Optional[Scope] = None
