7a953446898344833f6949adfcad6b628ce23786
Zach Schubert <zschubert@nautiluslabs.co>
2022-08-11 11:13:13 -0400

    fix amqp_invoker.py

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -21,7 +21,7 @@ isort $mod -c
 if [ $? -ne 0 ]; then printf "\e[1mFailed.\e[0m\n" ; exit_code=1; fi; printf "\n"
 
 printf "\e[1mType Checking (mypy) ...\e[0m\n"
-mypy --strict $mod
+mypy --strict --install-types $mod
 if [ $? -ne 0 ]; then printf "\e[1mFailed.\e[0m\n" ; exit_code=1; fi; printf "\n"
 
 printf "\e[1mComplexity check ...\e[0m\n"
